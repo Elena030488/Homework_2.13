e7bee6c02926ff6a2920569de27a5fea0d049dc6
Lenka <aruru.goddes@gmail.com>
2024-05-13 07:50:09 +0300

    Домашнее задание Spring и Mockito

---
+++ src/main/java/pro/sky/Homework_2/_Spring_and_Mockito/Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_2._Spring_and_Mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Homework_2/_Spring_and_Mockito/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework_2._Spring_and_Mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
